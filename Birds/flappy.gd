f36156c37b9afba4c5b46c0998ed0bfc3d0aefb4
Racer911-1 <102369191+Racer911dash1@users.noreply.github.com>
2024-05-29 11:54:45 +0200

    Add files via upload

--- Birds/flappy.gd
+++ Birds/flappy.gd
@@ -13,17 +13,19 @@ signal hit
 @onready var point_audio_stream_player: AudioStreamPlayer = $PointAudioStreamPlayer
 @onready var point_collect_2d: Area2D = $PointCollect2D
 
-@export var jump : float = 260
-
+var flappy_skin : Array = ["Fly_Yellow", "Fly_Red", "Fly_Blue"]
+var jump : float = 360
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-var dead: bool = false
 var apply_gravity_bool : bool = false
 
+func _ready() -> void:
+	random_bird_sprite()
+
 func _physics_process(delta: float) -> void:
-	global_position.x = 72
+	global_position.x = get_viewport_rect().size.x / 4
 	apply_gravity(delta)
 	
-	if dead == false:
+	if GameManager.flappy_died == false:
 		jumping()
 	move_and_slide()
 
@@ -34,8 +36,7 @@ func _on_hurtbox_area_2d_body_entered(body: Node2D) -> void:
 	dead_audio_stream_player.play()
 	hurtbox_area_2d.set_collision_mask_value(2, false)
 	point_collect_2d.set_collision_mask_value(3, false)
-	flappy_animation_player.play("Dead")
-	dead = true
+	flappy_animation_player.pause()
 	
 func jumping():
 	if Input.is_action_just_pressed("jump"):
@@ -51,3 +52,7 @@ func apply_gravity(delta):
 func _on_point_collect_2d_area_entered(area: Area2D) -> void:
 	point_audio_stream_player.play()
 	pipe_passed.emit()
+	
+func random_bird_sprite():
+	var random_flappy_skin = flappy_skin.pick_random()
+	flappy_animation_player.play(random_flappy_skin)
